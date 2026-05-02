06d47b84b119d1791cd7044ead2d1e4ac513bbde
zahid <zahidduron@gmail.com>
2026-05-01 21:51:36 -0600

    refactor: update App component and add custom font styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ function App() {
 
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="font-mono">
+      <h1>Hola mundo</h1>
+    </div>
   )
 }
 
