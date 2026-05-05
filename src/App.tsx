f3eafd31ec0bf6ebf198044d9dff29b7e3cb1382
zahid <zahidduron@gmail.com>
2026-05-04 21:54:23 -0600

    feat: implement dynamic filtering for extensions based on settings context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
+import { useMemo } from "react"
 import ExtensionCard from "./components/ExtensionCard"
 import { Filter } from "./components/Filter"
 import Header from "./components/Header"
 import { EXTENSIONS } from "./constants/data"
+import { useSettings } from "./context/SettingsContext"
 
 function App() {
+
+  const {settings} = useSettings()
+
+  const filteredExtensions = useMemo(() => {
+    return EXTENSIONS.filter(ex => {
+      if (settings.filter === "All") return true
+      if (settings.filter === "Active") return ex.isActive
+      if (settings.filter === "Inactive") return !ex.isActive
+      return false
+    })
+  }, [settings.filter])
+
+  
 
   return (
     <div className="font-mono bg-neutral-100 h-screen p-3 space-y-7 text-base">
       <Header />
       <Filter />
       <div className="flex flex-col gap-4">
-        {EXTENSIONS.map(extension => (
+        {filteredExtensions.map(extension => (
           <ExtensionCard key={extension.name} info={extension} />
         ))}
       </div>
